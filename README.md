cfa60ae57099ae5f7fb3ee6a090fabb6ec28103a
Nelson Moran <mm19037@ues.edu.sv>
2024-11-21 18:13:15 -0600

    Initial commit for University project

---
+++ README.md
@@ -0,0 +1,1 @@
+Univerity Project with .NET
---
+++ UniversityAPI/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Models.DataModels;
+
+namespace UniversityAPI.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options) { }
+
+
+        // TODO: Add DbSets (Tables of our database)
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
